c84e52af235d3ccd78e2be653c129761e9e653dc
Oluwatobiloba Williams <tobiwilliams003@gmail.com>
2026-08-03 23:26:57 +0100

    chore: configure tooling, formatting and project dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Image `remotePatterns` will be added here once the CMS is chosen.
 };
 
 export default nextConfig;
